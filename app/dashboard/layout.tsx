8d37fcd8ee38b41a4a43e02438e25e998c4c32b0
Valentin Korneev <v.korneef@gmail.com>
2024-09-22 21:07:26 +0600

    Chapter 16 Adding Metadata was completed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,15 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Dashboard',
+  },
+};
+
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
